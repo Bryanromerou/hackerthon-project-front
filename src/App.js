b388e844ec2ecc9442fbf471dd755ec813dad1b5
andyvdoan <andyy.doan@gmail.com>
2020-11-23 15:53:24 -0600

    Added Auth0 framework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <div>
+    <>
       <Navbar/>
-      App
-    </div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
   );
 }
 
