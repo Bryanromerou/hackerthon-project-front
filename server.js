08986a78dc3f2e9150d5b2bd99abafd8d03028e0
WillV1 <wmvanhook@gmail.com>
2020-11-24 12:38:19 -0500

    Stash changes

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = 4000
 
 //middleware -------------------------------
 app.use(bodyParser.json());
