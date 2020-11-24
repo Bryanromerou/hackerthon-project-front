70af1a1d2ab67341370e6c2b2ffa9446e61ab5be
WillV1 <wmvanhook@gmail.com>
2020-11-24 00:13:26 -0500

    Update profile

--- src/pages/CommunityFeed.js
+++ src/pages/CommunityFeed.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Container, Button, Link } from 'react-floating-action-button'
+
 
 const CommunityFeed = (props) => {
 
-  const discussionList = props.list.map((item, index) => {
-    return <li key={index}>{item}</li>
+  const discussionList = props.listTwo.map((itemTwo, index) => {
+    return <li key={index}>{itemTwo}</li>
   }) 
 
     return (
       <div>
-        <h2>Trending Discussions in </h2>
-        <ol>
-          {discussionList}
-        </ol>
         <h2>Top Discussions</h2>
+        {discussionList}
+        <Container>
+          <Button
+          tooltip="Create new discussion"
+          icon="far fa-edit" 
+          styles={{backgroundColor:darkColors.lighterRed}}
+          />
+        </Container>
       </div>
     )
 }
